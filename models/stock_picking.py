ee0ba5cf070ea81db3a71fc5a825be3fb6acbfe4
irtiza_nabio <irtizahossainnabio@gmail.com>
2025-06-24 14:25:52 +0600

    Third Commit

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api
-from odoo.exceptions import UserError
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -18,25 +17,10 @@ class StockPicking(models.Model):
     end_time = fields.Datetime("End Time")
     total_delivery_time = fields.Char("Total Delivery Time", compute="_compute_total_time", store=True)
 
-    delivery_started = fields.Boolean(
-        compute='_compute_delivery_started',
-        store=True
-    )
-
     delivery_method = fields.Selection([
         ('internal_delivery', 'Internal Delivery'),
         ('external_delivery', 'External Delivery'),
     ], string="Delivery Method")
-
-    def action_start_delivery(self):
-        for record in self:
-            record.start_time = fields.Datetime.now()
-
-    def action_end_delivery(self):
-        for record in self:
-            if not record.start_time:
-                raise UserError("Please start the delivery first.")
-            record.end_time = fields.Datetime.now()
 
     @api.depends('start_time', 'end_time')
     def _compute_total_time(self):
@@ -46,8 +30,3 @@ class StockPicking(models.Model):
                 record.total_delivery_time = str(diff)
             else:
                 record.total_delivery_time = ''
-
-    @api.depends('start_time')
-    def _compute_delivery_started(self):
-        for rec in self:
-            rec.delivery_started = bool(rec.start_time)
